4f32300332282885392debc117b7940d6ee09266
galid1 <galid1@naver.com>
2023-01-14 00:04:13 +0900

    added evictor

---
+++ src/main/kotlin/com/example/awstest/service/CacheEvictor.kt
@@ -0,0 +1,14 @@
+package com.example.awstest.service
+
+import org.springframework.cache.CacheManager
+import org.springframework.stereotype.Component
+
+@Component
+class CacheEvictor(
+    private val cacheManager: CacheManager
+) {
+    fun evict(id: Long) {
+        cacheManager.getCache("test_cache")
+            ?.run { this.evict(id) }
+    }
+}
--- src/main/kotlin/com/example/awstest/service/TestService.kt
+++ src/main/kotlin/com/example/awstest/service/TestService.kt
@@ -1,8 +1,7 @@
-package com.example.awstest.listener
+package com.example.awstest.service
 
 import com.example.awstest.jpa.TestEntity
 import com.example.awstest.jpa.TestRepository
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -10,14 +9,16 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class TestService(
     private val testRepository: TestRepository,
+    private val evictor: CacheEvictor
 ) {
     init {
         testRepository.save(TestEntity(name = "A"))
     }
 
-    @Cacheable(cacheNames = ["test_cache"], key = "#name")
-    fun service(name: String): String? {
-        return testRepository.findByName(name)
+    @Cacheable(cacheNames = ["test_cache"], key = "#id")
+    fun service(id: Long): String? {
+        return testRepository.findById(id)
+            .get()
             ?.let {
                 """
                         id: ${it.id}
@@ -29,6 +30,7 @@ class TestService(
     @Transactional
     fun update() {
         val bu  = testRepository.findByName("A")
+        evictor.evict(bu!!.id!!)
         bu?.name = "B"
     }
 }
